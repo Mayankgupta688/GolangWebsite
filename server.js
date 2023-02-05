28ddb89ac8910892424734bf0a6f37071bc4cdc0
MayankGupta <mayank.gupta.6.88@gmail.com>
2023-02-05 23:52:11 +0530

    Updates to Golang Links

--- server.js
+++ server.js
@@ -6,39 +6,57 @@ app.set("view engine", "vash");
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (_, res) {
-    res.render('introduction');
+    res.render('introduction', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 app.get('/introduction-to-golang', function (_, res) {
-    res.render('introduction');
+    res.render('introduction', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 app.get('/getting-stared-with-golang', function (_, res) {
-    res.render('gettingStarted');
+    res.render('gettingStarted', {
+        description: "Simple Steps to get started with Golang"
+    });
 });
 
 app.get('/hello-world-in-golang', function (_, res) {
-    res.render('golangHelloWorld');
+    res.render('golangHelloWorld', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 app.get('/creating-variables-in-golang', function (_, res) {
-    res.render('variablesInGolang');
+    res.render('variablesInGolang', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 app.get('/type-conversion-in-golang', function (_, res) {
-    res.render('typeConversion');
+    res.render('typeConversion', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 app.get('/constant-keyword-in-golang', function (_, res) {
-    res.render('constantKeywords');
+    res.render('constantKeywords', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 app.get('/iota-keyword-in-golang', function (_, res) {
-    res.render('iotaInGolang');
+    res.render('iotaInGolang', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });  
 
 app.get('/introduction-to-pointers-in-golang', function (_, res) {
-    res.render('workingWithPointers');
+    res.render('workingWithPointers', {
+        description: "Introduction to Golang, Getting started with Go/Golang"
+    });
 });
 
 
