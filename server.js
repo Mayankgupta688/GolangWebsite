b65de2aa7f469ae04c669aa2bacc0e2b22ace48f
MayankGupta <mayank.gupta.6.88@gmail.com>
2023-02-11 21:56:23 +0530

    Added Pages

--- server.js
+++ server.js
@@ -7,61 +7,67 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (_, res) {
     res.render('introduction', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "Getting started with Golang. Learn Golang in Easy Steps. Free Golang Articles and Concepts"
     });
 });
 
 app.get('/introduction-to-golang', function (_, res) {
     res.render('introduction', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "What is Golang, Why do we use Golang. Advantages and Disadvantages of Golang"
     });
 });
 
 app.get('/getting-stared-with-golang', function (_, res) {
     res.render('gettingStarted', {
-        description: "Simple Steps to get started with Golang"
+        description: "Software Installation required for Golang. Install software required for Golang"
     });
 });
 
 app.get('/hello-world-in-golang', function (_, res) {
     res.render('golangHelloWorld', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "Hello World Application with Golang. Get started with Golang in Minutes. "
     });
 });
 
 app.get('/creating-variables-in-golang', function (_, res) {
     res.render('variablesInGolang', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "How to create variables in Golang, Creating and using Golang Variables."
     });
 });
 
 app.get('/type-conversion-in-golang', function (_, res) {
     res.render('typeConversion', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "Type Conversion in Golang. Converting Different data types in Golang"
     });
 });
 
 app.get('/constant-keyword-in-golang', function (_, res) {
     res.render('constantKeywords', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "How to create and use Contants in Golang Language."
     });
 });
 
 app.get('/iota-keyword-in-golang', function (_, res) {
     res.render('iotaInGolang', {
-        description: "Introduction to Golang, Getting started with Go/Golang"
+        description: "IOTA keyword in Golang. Learn how to use IOTA and its Advantages"
     });
 });  
 
 app.get('/introduction-to-arrays-in-golang', function (_, res) {
     res.render('workingWithArrays', {
-        description: "Working with Arrays in Golang. Different Array Functions in Golang"
+        description: "Working with Arrays in Golang. Different Array Functions in Golang."
     });
 });
 
 app.get('/arrays-copied-by-value-in-golang', function (_, res) {
     res.render('copyingArrays', {
-        description: "Arrays are Copied via Value in Golang."
+        description: "Array comparison in Golang. Steps to equate 2 Arrays in Golang."
+    });
+});
+
+app.get('/passing-data-to-function', function (_, res) {
+    res.render('passingDataToFunctionParameter', {
+        description: "Sending data to function parameters in Golang. Data is sent to function parameter by Value"
     });
 });
 
